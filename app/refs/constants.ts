08adba3599235453fbd72f77a8b5d452d582a0cb
majortom327 <me@valentin-thomas.com>
2023-05-25 19:18:42 +0200

    Update constants

--- app/refs/constants.ts
+++ app/refs/constants.ts
@@ -1,8 +1,9 @@
 import cryptoDevsAbi from "./abi/CryptoDevs.json";
 
+
 export const pageSize = 10;
 export const abi = cryptoDevsAbi;
-export const contractAddress = "0xA3431f1643544755c9cA8Bf5bf778a915D16F733";
+export const contractAddress = "0x07d673447A6AB7a88f68f7AC45037d493C7e8b41";
 
 export default {
   pageSize,
